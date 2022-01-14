402aabed9d89bf3359135b03bed6c5a078670374
viet009 <minhviet810business@gmail.com>
2022-01-14 15:53:51 +0800

    fix logger

--- index.py
+++ index.py
@@ -1,13 +1,13 @@
 import logging, os
+logging.basicConfig(level=logging.DEBUG)
 
-from flask_restful import Resource
-# from src.api.resources.TokenProtected.Topics import Topics
+
 from src.api.models import *
 from dotenv import load_dotenv
-from src.server import app, api
+from src.server import app
 
 load_dotenv()
-logging.basicConfig(level=logging.DEBUG)
+
 
 if __name__ == "__main__":
     port = 5000
@@ -16,4 +16,3 @@ if __name__ == "__main__":
     from src.api import routes
 
     app.run(debug=True, port=port)
-
